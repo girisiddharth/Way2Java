6e0dcdae3c88206dabb6ae6b7fa26929df78cdc8
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-09-18 19:06:24 +0530

    Add files via upload
    got agents name

---
+++ input4.java
@@ -0,0 +1,35 @@
+/*Agent Binod
+You are required to take input a string agentName, and finally print the 
+line "Hi my name is Agent insert agentName".
+
+Input Format
+First line contains a string which is the name of the agent.
+
+Output Format
+One line output of format "Hi my name is Agent insert agentName"
+
+Example 1
+Input
+
+Binod
+Output
+
+Hi my name is Agent Binod
+Example 2
+Input
+
+Binod Bhushan
+Output
+
+Hi my name is Agent Binod Bhushan
+Constraints
+1 <= agentName.size <= 100000 */
+
+import java.util.*;
+public class input4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println("Hi my name is Agent "+a);
+    }
+}
