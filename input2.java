9dd751b4b720bfbd9006dbad6f2e5d1efc0466bf
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-09-18 18:44:26 +0530

    Add files via upload
    remove the comment line

---
+++ input2.java
@@ -0,0 +1,30 @@
+/*import java.util.*;
+ public class input2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte a = sc.nextByte();
+        byte b = sc.nextByte();
+        System.out.println(a+b);
+    }
+ }*/
+
+
+ /*import java.util.*;
+ public class input2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short a = sc.nextShort();
+        short b = sc.nextShort();
+        System.out.println(a+b);
+    }
+ }*/
+
+ import java.util.*;
+ public class input2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.println(a+b);
+    }
+ }
