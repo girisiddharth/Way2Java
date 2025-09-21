4944cb85b1d3ff77de28959c52b117a066bfe576
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-09-21 21:01:11 +0530

    Add files via upload
    while loop started with basic question

---
+++ whileloop1.java
@@ -0,0 +1,14 @@
+/*practice for while loop basic question  */
+
+import java.util.*;
+public class whileloop1{
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+    while(n>=1){
+    System.out.println(n);
+    n--;
+}
+
+}
+}
