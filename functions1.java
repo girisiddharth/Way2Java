97abe607f586d08af437721a4b1dcc634916122c
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-12 05:31:48 +0530

    Add files via upload
    the basic function code

---
+++ functions1.java
@@ -0,0 +1,12 @@
+
+public class functions1 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("hello");
+        sid();
+    }
+    static void sid(){
+       System.out.println("my name is sid");
+       System.out.println("i am 20 yrs old");
+    }
+    }
