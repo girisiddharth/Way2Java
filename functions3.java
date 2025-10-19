78547a40a80a883ea480d47ca11cba4104b8d7f4
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-19 06:09:44 +0530

    Add files via upload
    swapping numbers by using functions

---
+++ functions3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class functions3 {
+
+
+    public static void swap(int a,int b){
+        int c = a;
+         a = b;
+         b = c;
+     System.out.println("a="+ a);
+     System.out.println("b="+b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        swap(a,b);
+
+    }
+}
