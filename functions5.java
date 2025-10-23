7658713b9ad53b8433e30ea7f8fe04824eb2b666
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-23 15:27:43 +0530

    Add files via upload
    calling char by function

---
+++ functions5.java
@@ -0,0 +1,35 @@
+/*Function Problem 1
+Write a function such that a function fun will be called and 
+"I am another function" will be printed.
+
+Input Format
+none
+
+Output Format
+"I am another function" is to be printed without quotations as below
+
+Example 1
+Input
+
+Output
+
+I am another function */
+
+
+public class functions5{
+
+    public static void fun(){
+        
+        System.out.println("I am another function");
+
+        
+    }
+public static void main(String[] args) {
+    
+    fun();
+   
+}
+}
+
+
+
