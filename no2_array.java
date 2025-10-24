ba6e30b73f4e3f59976fc97030800f2d40992739
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-24 15:53:31 +0530

    Add files via upload
    //another way to put array
    
        //to print all index of subject array
        
        //to check the length of array

---
+++ no2_array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class no2_array {
+  public static void main(String[] args) {
+    //another way to put array
+    String [] subjects = {"maths","physics","chemistry","DSA"};
+    //to print all index of subject array
+    System.out.println(Arrays.toString(subjects));
+    //to print any index of array
+    System.out.println(subjects[0]);
+    System.out.println(subjects[1]);
+    System.out.println(subjects[2]);
+    System.out.println(subjects[3]);
+    //to check the length of array
+    System.out.println(subjects.length);
+  }  
+}
