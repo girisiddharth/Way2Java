7a215b7c71030fb0f5efd764c92e8fed484510cb
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-24 15:31:19 +0530

    Add files via upload
    declare and initialize the first code basic need for learning array

---
+++ no1_array.java
@@ -0,0 +1,14 @@
+//declear and initialize the code 
+
+import java.util.*;
+public class no1_array {
+    public static void main(String[] args) {
+        int [] arr  = new int[5];
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+
+        String [] name = new String [10];
+        System.out.println(name);
+        System.out.println(Arrays.toString(name));
+    }
+}
