87a418eba54c74b1f0fb1f1422835578b884693c
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-26 18:40:54 +0530

    Add files via upload
    linear search for first digit that comes of user input

---
+++ Lsearch.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Lsearch {
+
+    public static int linearsearch(int [] arr,int target){
+        for (int idx = 0; idx < arr.length;idx++ ){
+            if (arr[idx]==target){
+                return idx;
+
+            }
+        }
+        return -1;
+    }
+        
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("put size");
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+        System.out.println("put value");
+        for (int idx = 0; idx<arr.length;idx++){
+            
+            arr[idx] = sc.nextInt();
+        }
+        System.out.println("put the target");
+            int target = sc.nextInt();
+        int index = linearsearch(arr,target);
+        if (index == -1){
+            System.out.println("not found");
+        }
+        else{ 
+            System.out.println("index of "+ target + " is "+ index);
+        }
+        }
+    
+    }
+
+
