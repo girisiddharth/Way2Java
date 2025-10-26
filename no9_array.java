9b7748aed41bc703ece14d8b5eb44b2c96082922
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-26 12:32:11 +0530

    Add files via upload
    calling full array

---
+++ no9_array.java
@@ -0,0 +1,17 @@
+
+
+
+import java.util.*;
+public class no9_array {
+    public static int[] getArr(){
+int [] nums = {1,2,3};
+return nums;
+    }
+    public static void main(String[] args) {
+        int [] arr = getArr();
+System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+
