1dc6981889bfd3f089b441a1d67ebe5fcd53b40a
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-10-26 12:33:03 +0530

    Add files via upload
    single calling functions

---
+++ no10_array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class no10_array {
+    public static int getArr(){
+int [] nums = {1,2,3};
+return nums[0];
+    }
+    public static void main(String[] args) {
+        int value = getArr();
+System.out.println(value);
+    }
+}
