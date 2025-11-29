100a059c75874ab328f06c226a8bc68d3aadae3d
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-11-29 09:38:43 +0530

    Add files via upload
    easy

---
+++ Lsearch15.java
@@ -0,0 +1,76 @@
+/**Largest Number At Least Twice of Others
+You are given an integer array nums of size n where the largest integer
+ is unique. Your task is to determine whether the largest element in the
+  array is at least twice as much as every other number in the array. 
+  If it is, print the index of the largest element, or print -1 otherwise.
+
+Input Format
+The first line of the input contains the number n(length of the array),
+
+The next line contains n space separated integers denoting the elements of the array.
+
+Output Format
+Print the index of the element if it satisfies the condition else print -1
+
+Example 1
+Input
+
+4
+3 6 1 0
+Output
+
+1
+Explanation
+
+Here, 6 is the largest integer, and for every other number in the array x, 
+6 is more than twice as big as x. The index of value 6 is 1, so we return 1
+
+Example 2
+Input
+
+4
+1 2 3 4
+Output
+
+-1
+Explanation
+
+4 isn't at least as big as twice the value of 3, so we return -1 */
+
+import java.util.*;
+public class Lsearch15{
+    public static void nextstep(int arr[],int n){
+        int largest = Integer.MIN_VALUE;
+        int slargest= Integer.MIN_VALUE;
+        int maxindex=-1;
+
+
+        for (int idx=0;idx<arr.length;idx++){
+             if (arr[idx]>largest){
+                slargest=largest;
+                largest = arr[idx];
+             } 
+             else if (arr[idx]>slargest){
+                slargest = arr[idx];
+             }
+        }
+        if (slargest>=largest*2){
+            System.out.println(maxindex);
+        }
+        else{
+            System.out.println("-1");
+        }
+    }
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n= sc.nextInt();
+    int arr[]= new int[n];
+    for(int idx=0;idx<arr.length;idx++){
+        arr[idx] = sc.nextInt();
+
+    }
+    nextstep(arr,n);
+
+  }
+
+}
