01a45e577b0d3a78988b3e6857c99e83ca4735ae
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-12-02 23:13:02 +0530

    Add files via upload

---
+++ twoD1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class twoD1{
+    public static void main (String[]args){
+         Scanner sc = new Scanner (System.in);
+         int n = sc.nextInt();
+         int m = sc.nextInt();
+         int mat[][] = new int [n] [m];
+         for (int row = 0;row<mat.length;row++){
+             for (int col=0;col<mat[row].length;col++){
+                 mat[row][col] = sc.nextInt();
+             }
+         }
+         for (int row=0;row<mat.length;row++){
+            for (int col=0;col<mat[row].length;col++){
+                System.out.print(mat[row][col]+" ");
+            }
+            System.out.println();
+         }
+    }
+}
