42624c67db55aa9179a592ce8f37c873f72c60f6
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-12-17 03:28:50 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,16 @@
+
+//concatination
+
+public class string2 {
+    public static void main(String[]args){
+        String str = "sid";
+        String str2 = "goswami";
+
+        System.out.print(str + " " + str2+ " ");
+
+        str+=" " +  "goswami";
+         System.out.print(str);
+        
+    }
+    }
+
