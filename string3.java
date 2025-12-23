bde5a206949f2c3d1f1354e37ed40bd5d999ed7a
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-12-24 01:16:16 +0530

    Add files via upload

---
+++ string3.java
@@ -0,0 +1,7 @@
+public class string3 {
+    public static void main(String[]args){
+        String n = "jdhggbvfkgvkjchgsbyuxgfmjgsabysdmcsahgbduowgbcsdgoatmkbfjgjhdsgfjh";
+        System.out.println(n.substring(50,58));
+
+    }
+}
